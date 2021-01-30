f86278ad7d9f8f8a1c4dc6290ef32baf811e4d45
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2021-01-30 08:11:47 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import stringSimilarity from "string-similarity";
+import { useState, useEffect } from "react";
+import ExcelJS from "exceljs";
+import { CSVLink } from "react-csv";
+import Form from "react-bootstrap/Form";
 
 function App() {
+  const [file, setFile] = useState(null);
+  const [isProcessed, setProcessed] = useState(false);
+  const [output, setOutput] = useState([]);
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    setFile(event.target.files[0]);
+  };
+
+  const handleUpload = async (file) => {
+    let values = [["", "Application Name", "Momo Name", "Name Score"]];
+    const workbook = new ExcelJS.Workbook();
+    await workbook.xlsx.load(file);
+    const worksheet = workbook.getWorksheet("Sheet1");
+    worksheet.eachRow((row, rowNumber) => {
+      values.push(row.values);
+      values[values.length - 1].push(
+        Math.round(
+          stringSimilarity.compareTwoStrings(row.values[1], row.values[2]) * 100
+        )
+      );
+    });
+    setProcessed(true);
+    setOutput(values);
+  };
+
+  const resetState = () => {
+    setProcessed(false);
+    setOutput([]);
+    setFile(null);
+  };
+
+  const fileDownload = (
+    <CSVLink data={output} onClick={resetState}>
+      Download me
+    </CSVLink>
+  );
+
+  useEffect(() => {
+    file && handleUpload(file);
+  }, [file]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" onChange={handleChange}></input>
+      <Form>
+        <Form.File id="custom-file" label="Custom file input" onChange={handleChange} custom />
+      </Form>
+      {isProcessed && output.length !== 0 && fileDownload}
     </div>
   );
 }
